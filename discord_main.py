f6feeae71e8e405219422e67f38fa9c844294338
Taiiwo Llort <Taiiwo@Llort.gq>
2020-07-01 00:02:03 +0100

    send the server specific config to the plugins

--- discord_main.py
+++ discord_main.py
@@ -6,7 +6,8 @@ if len(sys.argv) == 2:
     config = config.get_config(config_location=sys.argv[1])
 else:
     config = config.get_config()
+config = config["discord_config"]
 # use discord as our server protocol
-server = discord.Discord(config['discord_config'])
+server = discord.Discord(config)
 # Start the bot!
 taiiwobot.TaiiwoBot(server, config)
