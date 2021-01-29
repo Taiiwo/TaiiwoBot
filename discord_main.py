52e17905289eca0ef77ce3a2ae0a21fe7690d734
Taiiwo <Taiiwo@Llort.gq>
2021-01-29 09:58:42 +0000

    Make config into a class and add an update method

--- discord_main.py
+++ discord_main.py
@@ -3,10 +3,9 @@ from taiiwobot import taiiwobot, discord, config, util
 import sys
 
 if len(sys.argv) == 2:
-    config = config.get_config(config_location=sys.argv[1])
+    config = config.Config(config_location=sys.argv[1], key="discord_config")
 else:
-    config = config.get_config()
-config = config["discord_config"]
+    config = config.Config(key="discord_config")
 # use discord as our server protocol
 server = discord.Discord(config)
 # Start the bot!
--- taiiwobot/config.py
+++ taiiwobot/config.py
@@ -2,34 +2,45 @@ import json
 from . import irc
 
 
-def get_config(config_location="config.json"):
-    default_config = {
-        "irc_config": {
-            "type": "irc",
-            "host": "example.com",
-            "user": "TaiiwoBot",
-            "nick": "TaiiwoBot",
-            "ident": "TaiiwoBot",
-            "autojoin": [],
-        },
-        "discord_config": {
-            "owner[REMOVE]": "your user ID",
-            "api_key[REMOVE]": "Insert your api key here and remove the [REMOVE] tag from the key <--",
-        },
-    }
-    try:
-        user_config = json.loads(open(config_location).read())
-    except (IOError, OSError):
-        answer = input(
-            "No config file was found. Would you like to generate one? (y/N)"
-        )
-        if answer[0].lower() == "y":
-            open(config_location, "w+").write(
-                json.dumps(default_config, indent=4, separators=(",", ": "))
+class Config(dict):
+    def __init__(self, config_location="config.json", key=False):
+        self.config_location = config_location
+        self.key = key
+        default_config = {
+            "irc_config": {
+                "type": "irc",
+                "host": "example.com",
+                "user": "TaiiwoBot",
+                "nick": "TaiiwoBot",
+                "ident": "TaiiwoBot",
+                "autojoin": [],
+            },
+            "discord_config": {
+                "owner[REMOVE]": "your user ID",
+                "api_key[REMOVE]": "Insert your api key here and remove the [REMOVE] tag from the key <--",
+            },
+        }
+        try:
+            user_config = json.loads(open(config_location).read())
+        except (IOError, OSError):
+            answer = input(
+                "No config file was found. Would you like to generate one? (y/N)"
             )
-            print("[i] A config file was created. Edit it and try again")
-        else:
-            print("[i] A config file is required to run TaiiwoBot")
-        quit()
-    default_config.update(user_config)
-    return default_config
+            if answer[0].lower() == "y":
+                open(config_location, "w+").write(
+                    json.dumps(default_config, indent=4, separators=(",", ": "))
+                )
+                print("[i] A config file was created. Edit it and try again")
+            else:
+                print("[i] A config file is required to run TaiiwoBot")
+            quit()
+        default_config.update(user_config)
+        for k, v in default_config[key].items():
+            if self.key:
+                super().__setitem__(k, v)
+            else:
+                super().__setitem__(k, v)
+
+    def save_config(self):
+        with open(self.config_location, "w") as f:
+            f.write(json.dumps(self, indent=4))
